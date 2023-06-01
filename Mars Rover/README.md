0f7417292fe165f51771e237c94a53c2b09b2e8a
ayush2342 <ayushsharma2342@gmail.com>
2023-06-01 21:13:29 +0530

    Mars Rover Done

--- Mars Rover/README.md
+++ Mars Rover/README.md
@@ -1,2 +1,3 @@
-
+This project is made to fetch the images captured by the Mars Rover on a date basis.
+NASA API - https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY
 Project is made using HTML, CSS ,JQUERY, JAVASCRIPT using the AJAX functionality
---
+++ Mars Rover/script.js
@@ -0,0 +1,59 @@
+
+  $(document).ready(function()
+  {
+    let datebox=$("#datepicker");
+    $(function() {
+        datebox.datepicker({
+            dateFormat: "yy-mm-dd",
+            maxDate: "0"
+        });
+      });
+
+      let imageClick = $('#get-Image');
+      let ImageDiv = $('#image-div')
+    
+    
+      imageClick.click(function()
+      {
+        let date=datebox.val();
+
+        if(date)
+        {
+            $.ajax(
+                {
+                    url:'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+                    method:'get',
+                    data:
+                    {
+                        earth_date:date,
+                        api_key:'FW6SPwj4WcoTFeqxkgV2EXaKe7xujSYTyQXJsliT'
+                    },
+                    success:function(data)
+                    {
+                        if(data.photos.length==0)
+                        {
+                            window.alert("Images not yet uploaded by NASA for the selected Date. Please select any previous dates");
+                            return;
+                        }
+                        for(let pictures of data.photos)
+                        {
+                            let newImage = `<img src="${pictures.img_src}" id="imageID" >`
+                            ImageDiv.append(newImage);
+                        }
+                    }
+
+                }
+            ).fail(function(error)
+            {
+               console.log("Error Occured -",error)
+            })
+
+        }
+        else 
+        {
+            window.alert("Please enter Date")
+        }
+        
+      })
+      
+  })
